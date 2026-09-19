418d11a008194f8fd72fb5fb19b202b449598bd0
bbn1012 <baonam2k7@gmail.com>
2026-09-19 15:49:36 +0700

    e

--- lab2.js
+++ lab2.js
@@ -2,8 +2,8 @@
 const names = ["An", "Bình", "Nam", "Hòa", "Lan"];
 
 console.log(names)
-console.log("Phần tử đầu tiên: ",names[0]);
-console.log("Phần tử cuối cùng: ",names[4]);
+console.log("Phần tử đầu tiên: ", names[0]);
+console.log("Phần tử cuối cùng: ", names[4]);
 console.log("Số lượng phần tử: ", names.length);
 
 names.unshift("Uyên");
@@ -15,6 +15,32 @@ console.log(names);
 // Bài 2
 const students = ["An", "Bình", "Nam", "Hòa", "Lan"];
 
-for (let i = 0; i < students.length; i++){
+for (let i = 0; i < students.length; i++) {
     console.log(students[i]);
+}
+
+// Bài 3
+const student = { id: 1, name: "Nguyễn Văn An", age: 20, email: "an@gmail.com", major: "CNTT", };
+
+console.log(student);
+console.log("Tên: ", student.name);
+console.log("Tuổi: ", student.age);
+console.log("Email: ", student.email);
+
+student.age = 22;
+console.log(student.age);
+
+student.phone = 012345678;
+console.log(student.phone);
+
+// Bài 4
+const stds = [{ id: 1, name: "Nguyễn Văn An", age: 20, }, { id: 2, name: "Trần Văn Bình", age: 21, }, { id: 3, name: "Lê Văn Nam", age: 20, },];
+
+console.log(stds);
+
+console.log("Tên sinh viên đầu tiên: ", stds[0].name);
+console.log("Tên sinh viên thứ 2: ", stds[1].name);
+
+for (let i = 0; i < stds.length; i++) {
+  console.log(stds[i].name);
 }
