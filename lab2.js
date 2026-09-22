34bfd0522ff0c744a8ba950e6e440d5d5631ceda
bbn1012 <baonam2k7@gmail.com>
2026-09-22 15:22:29 +0700

    h

--- lab2.js
+++ lab2.js
@@ -44,3 +44,76 @@ console.log("Tên sinh viên thứ 2: ", stds[1].name);
 for (let i = 0; i < stds.length; i++) {
   console.log(stds[i].name);
 }
+
+// Bài 5
+const studentss = [
+  {
+    id: 1,
+    name: "Nguyễn Văn An",
+    age: 20,
+  },
+  {
+    id: 2,
+    name: "Trần Văn Bình",
+    age: 21,
+  },
+  {
+    id: 3,
+    name: "Lê Văn Nam",
+    age: 20,
+  },
+];
+let html = document.getElementById("students");
+let content = "";
+for (let i = 0; i < studentss.length; i++) {
+  content += `
+    <div>
+      <h3>Ten sinh vien: ${studentss[i].name}</h3>
+      <h4>Tuổi: ${studentss[i].age}</h4>
+    </div>
+  `;
+}
+html.innerHTML = content;
+
+// Bài tổng hợp
+const products = [
+{
+id: 1,
+name: "iPhone 15",
+price: 20000000,
+},
+{
+id: 2,
+name: "Samsung Galaxy S24",
+price: 18000000,
+},
+{
+id: 3,
+name: "Xiaomi 14",
+price: 12000000,
+},
+];
+
+console.log(products);
+
+let totalPrice = 0;
+
+for (let i = 0; i < products.length; i++){
+    console.log(products[i].name + " - " + products[i].price);
+    totalPrice = totalPrice + products[i].price;
+}
+
+console.log("Tổng tiền: ", totalPrice);
+
+let html1 = "";
+
+for (let i = 0; i < products.length; i++) {
+    html1 += `
+        <p>${products[i].name} - ${products[i].price}</p>
+    `;
+}
+    html1 += `
+        Tổng tiền: ${totalPrice}
+    `;
+
+document.getElementById("products").innerHTML = html1;
