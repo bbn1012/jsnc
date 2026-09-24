0add3cca987972d31230461967661bb4675f6560
bbn1012 <baonam2k7@gmail.com>
2026-09-24 14:38:00 +0700

    u

---
+++ lab3.js
@@ -0,0 +1,29 @@
+// Bài 1
+const name = "Nguyễn Văn An";
+const age = 20;
+const major = "Lập trình Web";
+
+const result = `
+    Xin chào : ${name}
+    Bạn ${age} tuổi
+    Ngành học: ${major}
+    `;
+
+console.log(result);
+
+// Bài 2
+const student = {
+  id: 1,
+  name: "Nguyễn Văn An",
+  age: 20,
+  major: "Lập trình Web",
+};
+
+const resultStudent = `
+    Mã sinh viên: ${student.id}
+    Họ tên: ${student.name}
+    Tuổi: ${student.age}
+    Ngành: ${student.major}
+`;
+
+console.log(resultStudent);
---
+++ lesson3.js
@@ -0,0 +1,4 @@
+// Template Literal
+const name = "bbn";
+const age = 18;
+const info = "Ten: " + name + " Tuoi: " + age;
