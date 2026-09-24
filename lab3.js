0faaa0eff147c5d3fd966ea9d30fec41f7ca1ac2
bbn1012 <baonam2k7@gmail.com>
2026-09-24 15:46:00 +0700

    u

--- lab3.js
+++ lab3.js
@@ -27,3 +27,182 @@ const resultStudent = `
 `;
 
 console.log(resultStudent);
+
+// Bài 3
+const add = (a, b) => (a + b);
+
+console.log(add(3,4));
+
+// Bài 4
+const square = (a) => (a*a);
+console.log (square(5));
+
+const sum = (a, b) => (a + b);
+console.log(sum(4,5));
+
+const sayHello = (name) => `Xin chào: ${name}`;
+console.log(sayHello("Nam"))
+
+// Bài 5
+const numbers = [1, 2, 3, 4, 5];
+const newNumbers = numbers.map((number) => number * 2);
+console.log(newNumbers);
+
+// Bài 6
+const students = [
+  { id: 1, name: "An", age: 20 },
+  { id: 2, name: "Bình", age: 21 },
+  { id: 3, name: "Cường", age: 22 },
+];
+
+const names = students.map((student) => student.name);
+console.log(names);
+
+// Bài 7
+// const students = [
+//   {
+//     id: 1,
+//     name: "An",
+//     age: 20,
+//   },
+//   {
+//     id: 2,
+//     name: "Bình",
+//     age: 21,
+//   },
+//   {
+//     id: 3,
+//     name: "Cường",
+//     age: 22,
+//   },
+// ];
+
+document.getElementById("students").innerHTML = students
+  .map(
+    (student) => {
+    return `
+      <tr class="hover:bg-gray-50">
+              <td class="px-4 py-2 border border-gray-300">${student.id}</td>
+              <td class="px-4 py-2 border border-gray-300">${student.name}</td>
+              <td class="px-4 py-2 border border-gray-300">${student.age}</td>
+              <td class="px-4 py-2 border border-gray-300">
+                <div class="flex items-center justify-center gap-2">
+                  <a
+                    href="#"
+                    class="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded"
+                  >
+                    Edit
+                  </a>
+
+                  <button
+                    class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+    `;
+  })
+  .join("");
+
+  // Bài 8
+  const products = [
+  {
+    id: 1,
+    name: "iPhone 15",
+    price: 20000000,
+  },
+  {
+    id: 2,
+    name: "MacBook Air",
+    price: 25000000,
+  },
+  {
+    id: 3,
+    name: "AirPods",
+    price: 5000000,
+  },
+];
+
+
+document.getElementById("products").innerHTML = products
+  .map(
+    (product) => {
+    return `
+      <tr class="hover:bg-gray-50">
+              <td class="px-4 py-2 border border-gray-300">${product.id}</td>
+              <td class="px-4 py-2 border border-gray-300">${product.name}</td>
+              <td class="px-4 py-2 border border-gray-300">${product.price}</td>
+              <td class="px-4 py-2 border border-gray-300">
+                <div class="flex items-center justify-center gap-2">
+                  <a
+                    href="#"
+                    class="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded"
+                  >
+                    Edit
+                  </a>
+
+                  <button
+                    class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+    `;
+  })
+  .join("");
+
+  // Bài tổng hợp
+  const productsList = [
+  {
+    id: 1,
+    name: "Áo thun",
+    price: 150000,
+    category: "Thời trang",
+  },
+  {
+    id: 2,
+    name: "Quần jean",
+    price: 350000,
+    category: "Thời trang",
+  },
+  {
+    id: 3,
+    name: "Giày sneaker",
+    price: 800000,
+    category: "Giày",
+  },
+];
+
+document.getElementById("products-List").innerHTML = productsList
+  .map(
+    (product) => {
+    return `
+      <tr class="hover:bg-gray-50">
+              <td class="px-4 py-2 border border-gray-300">${product.id}</td>
+              <td class="px-4 py-2 border border-gray-300">${product.name}</td>
+              <td class="px-4 py-2 border border-gray-300">${product.price}</td>
+              <td class="px-4 py-2 border border-gray-300">${product.category}</td>
+              <td class="px-4 py-2 border border-gray-300">
+                <div class="flex items-center justify-center gap-2">
+                  <a
+                    href="#"
+                    class="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded"
+                  >
+                    Edit
+                  </a>
+
+                  <button
+                    class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+    `;
+  })
+  .join("");
